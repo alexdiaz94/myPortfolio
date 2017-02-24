4c8d39cf745b16c30d4e5c1242cc17e16495844d
alex diaz <alexdiaz094@gmail.com>
2017-02-24 13:06:53 -0500

    add sideMenuPic

--- public/js/init.js
+++ public/js/init.js
@@ -4,13 +4,6 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
-
-// $('#download-button').on('click', function() {
-
-//   var audio = new Audio("js/fart1.mp3" ) ;
-//   audio.play();
-//   // console.log('lcick')
-// })
  }); // end of document ready
 })(jQuery); // end of jQuery name space
 
